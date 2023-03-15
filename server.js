c71091157de36cf86e840830add6bdd2a6f57e18
Supriya Rani <68589808+Supriyabce@users.noreply.github.com>
2023-03-15 22:29:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config()
 connectDB();
 
 //esmodule fix
-const __filename = fileURLtoPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 //rest object
